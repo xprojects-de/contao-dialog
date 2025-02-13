67c2b9adad95498c58fa21a6b7bf7f56c9503024
Benjamin Hummel <infos@x-projects.de>
2025-02-13 09:14:25 +0100

    init

--- src/Controller/DialogModuleController.php
+++ src/Controller/DialogModuleController.php
@@ -28,30 +28,8 @@ class DialogModuleController extends AbstractFrontendModuleController
         $GLOBALS['TL_BODY'][] = '<script src="' . $this->packages->getUrl('alpdeskDialog.js', 'alpdesk_dialog') . '"></script>';
         $GLOBALS['TL_CSS'][] = $this->packages->getUrl('alpdeskDialog.css', 'alpdesk_dialog');
 
-        /*$template->id = $model->id;
-
-        switch ($model->overlay_loadingTime) {
-            case 'onLeave':
-                $GLOBALS['TL_BODY'][] = '<script src="' . $this->packages->getUrl('plentatooltip/overlay_showOnExit.js', 'plentatooltip') . '"></script>';
-                break;
-            case 'onLoad':
-                $GLOBALS['TL_BODY'][] = '<script src="' . $this->packages->getUrl('plentatooltip/overlay_showOnPageLoad.js', 'plentatooltip') . '"></script>';
-                break;
-            case 'afterTime':
-                $GLOBALS['TL_BODY'][] = '<script src="' . $this->packages->getUrl('plentatooltip/overlay_showAfterTime.js', 'plentatooltip') . '"></script>';
-                $template->data_delay = $model->overlay_delay;
-                break;
-            case 'afterScroll':
-                $GLOBALS['TL_BODY'][] = '<script src="' . $this->packages->getUrl('plentatooltip/overlay_showAfterScroll.js', 'plentatooltip') . '"></script>';
-                $template->data_percent = $model->overlay_percent;
-                break;
-        }
-
-        $GLOBALS['TL_CSS'][] = $this->packages->getUrl('plentatooltip/overlay_default.css', 'plentatooltip');
-
-        $template->data_expires = $model->overlay_cookie_expires;
-        $template->overlay_html = $model->overlay_html;
-        $template->overlay_richtext = $model->overlay_richtext;*/
+        $template->dialogId = $model->id;
+        $template->dialogContent = ($model->unfilteredHtml ?? '');
 
         return $template->getResponse();
     }
--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -6,6 +6,7 @@ use Alpdesk\AlpdeskDialog\Controller\DialogModuleController;
 
 $GLOBALS['TL_DCA']['tl_module']['palettes'][DialogModuleController::TYPE] = '
   {title_legend},name,headline,type;
+  {html_legend},unfilteredHtml;
   {template_legend:hide},customTpl;
   {expert_legend:hide},cssID
 ';
