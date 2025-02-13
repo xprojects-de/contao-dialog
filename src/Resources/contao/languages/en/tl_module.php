c6d2bac520927db4783a3fa64f50e4803c9fdf42
Benjamin Hummel <infos@x-projects.de>
2025-02-13 09:33:39 +0100

    init

---
+++ src/Resources/contao/languages/en/tl_module.php
@@ -0,0 +1,6 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['tl_module']['alpdeskDialogText'] = array('Text', 'Dialog-Content - Text');
+$GLOBALS['TL_LANG']['tl_module']['alpdeskDialogUnfilteredHtml'] = array('Html', 'Dialog-Content - unfiltered HTML');
